db4a430c3c22d561a1b92c5a488a97b7436fc118
Iamneo <techadmin@iamneo.ai>
2023-08-04 12:58:39 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/Booking.java
+++ jspservlet-app-binarytravels/src/main/java/services/Booking.java
@@ -2,5 +2,11 @@ package services;
 
 public class Booking{
 //	Enter code here...
-}
 
+
+    public double booking(int noOfPersons, int rates) {
+        return noOfPersons * rates;
+    }
+
+
+}
--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -1,12 +1,33 @@
 package services;
 
-import java.time.temporal.ChronoUnit;
-
 import model.Bus;
 import model.Flight;
 import model.Hotel;
 import model.Train;
 
-public class FareCalculator extends Booking{
-//Enter code here...
+public class FareCalculator extends Booking {
+    
+    public double book(Hotel hotel) {
+        int noOfPersons = hotel.getNoOfPersons();
+        int rates = hotel.getRates();
+        return booking(noOfPersons, rates);
+    }
+
+    public double book(Flight flight) {
+        int noOfPersons = flight.getNoOfPersons();
+        int rates = flight.getRates();
+        return booking(noOfPersons, rates);
+    }
+
+    public double book(Train train) {
+        int noOfPersons = train.getNoOfPersons();
+        int rates = train.getRates();
+        return booking(noOfPersons, rates);
+    }
+
+    public double book(Bus bus) {
+        int noOfPersons = bus.getNoOfPersons();
+        int rates = bus.getRates();
+        return booking(noOfPersons, rates);
+    }
 }
